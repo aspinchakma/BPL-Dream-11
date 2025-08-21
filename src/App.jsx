c1b535ce4566b90bfe599fd9885738ead05bbbde
Aspin Chakma <aspinchakma2019@gmail.com>
2025-08-22 00:56:33 +0600

    setup basic route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Fixture from "./Pages/Fixture";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import Schedules from "./Pages/Schedules";
+import Teams from "./Pages/Teams";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fixture" element={<Fixture />} />
+        <Route path="/schedules" element={<Schedules />} />
+        <Route path="/teams" element={<Teams />} />
+        {/* Catch All Routes */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h3>Not Found Pages</h3>
+    </div>
+  );
+};
+
+export default NotFound;

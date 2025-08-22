1e7f24b4dcdb952d3e29bd1a28f11e36eda5020b
Aspin Chakma <aspinchakma2019@gmail.com>
2025-08-22 12:45:50 +0600

    finished banner section

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,7 +1,23 @@
+import "./Banner.css";
 const Banner = () => {
   return (
-    <div>
-      <h3>This is Banner Section</h3>
+    <div className="banner_container lg:py-[64px] py-[40px]">
+      <img
+        className="mx-auto w-[248px]"
+        src="https://i.ibb.co.com/Q32hVYpj/banner-main.png"
+        alt=""
+      />
+      <h1 className="text-center text-white text-2xl lg:text-4xl font-bold mt-6 mb-4">
+        Assemble Your Ultimate Dream 11 Cricket Team
+      </h1>
+      <p className="text-center text-[#b9b9b9] text-xl lg:text-2xl">
+        Beyond Boundaries Beyond Limits
+      </p>
+      <div className="text-center mt-6">
+        <div className="button_container">
+          <button>Claim Free Credit</button>
+        </div>
+      </div>
     </div>
   );
 };
